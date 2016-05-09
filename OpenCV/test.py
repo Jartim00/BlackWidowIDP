b482a4b61f2ce3d59f3d212a97acd9d929b242a5
Jurjen <haze1401@student.nhl.nl>
2016-05-09 15:20:41 +0200

    begin van lock on

--- OpenCV/test.py
+++ OpenCV/test.py
@@ -4,6 +4,9 @@ import math
 import numpy as np
 
 class Camera:
+	lastPosition = None
+	framesRequired = 10
+	count = 0
 	def start(self):
 		cap = cv2.VideoCapture(0)
 		while(1):
@@ -11,38 +14,40 @@ class Camera:
 			ref, frame = cap.read()
 			hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 			lower_red = np.array([0, 50, 100])
-			upper_red = np.array([10, 255, 255])
+			upper_red = np.array([6, 255, 255])
 			mask0 = cv2.inRange(hsv, lower_red, upper_red)
-			lower_red = np.array([160,50, 100])
+			lower_red = np.array([170,50, 100])
 			upper_red = np.array([200,255,255])
 			mask1 = cv2.inRange(hsv, lower_red, upper_red)
 
 			mask = mask0+mask1
-   			#mask = cv2.bitwise_or(mask, frame)
 
 			kernel = np.ones((10,10),np.uint8)
 			mask =  cv2.erode(mask,kernel,iterations = 1)
 
-			dilation = np.ones((10, 10), "uint8")
+			dilation = np.ones((8, 8), "uint8")
 			mask = cv2.dilate(mask, dilation)
 
 			mask = cv2.medianBlur(mask,5)
 
-			circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1.4,50,param1=80,param2=70,minRadius=1, maxRadius=36000)
+			circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1.9,50,param1=80,param2=70,minRadius=1, maxRadius=3600)
 			if circles is None:
 				cv2.imshow('mask',mask)
-				#cv2.imshow('raw',frame)
-				#cv2.waitKey(1) #system sleep anders verdwijnt screen
+				count = 0
 				continue
 
 			#circles = np.uint16(np.around(circles))
 			for i in circles[0,:]:
+				lastPosition = i
 				cv2.circle(mask,(i[0],i[1]),2,(0,0,255),3)
 				cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
 
 
-			cv2.imshow('mask',mask)
-			cv2.imshow('frame',frame)
+			cv2.imshow('maskShot',mask)
+			cv2.imshow('frameShot',frame)
+                        count += 1
+			
+			print "X: "+ str(lastPosition[0]) + " Y: " + str(lastPosition[1]) + " radius: " + str(lastPosition[2])
 
 			k = cv2.waitKey(5) & 0xFF
 			if k == 27:
