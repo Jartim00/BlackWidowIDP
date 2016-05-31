9befac69b1fea844b6d8ded49e9bd32b9604eb1b
Diamundo <nautakevin@hotmail.com>
2016-05-31 10:55:43 +0200

    Walking code in progress

--- Pi_Servos/kevin/walking.py
+++ Pi_Servos/kevin/walking.py
@@ -29,7 +29,7 @@ class Foot :
 		ax.setLedStatus(10*self.legNr+2, status)
 		ax.setLedStatus(10*self.legNr+3, status)
 		
-	def step(self, deg) :
+	def step(self, id, deg) :
 		_FACTOR = 0.29296875 #300/1024 -> omrekenfactor van graden naar stappen
 		step = int(512 + (deg / _FACTOR))
 		if step < 0 : return 0
@@ -39,19 +39,20 @@ class Foot :
 	def time(self, id, loc) :
 		cur = ax.readPosition(id)
 		dx = abs(loc - cur) #get the distance to move
-		return spd
+		return dx #int(1024 * (dx/140))
 	
 	#self: this class. Not needed in call
 	#id : servo ID
 	#loc: end location
 	#spd: speed to use (not used yet, is computed)
 	def moveSpeedRW(self, id, loc) :
+		loc = self.step(id, loc) #check if valid location
+		spd = self.time(id, loc) #make valid time
 		if (
 			id in ax.learnServos(10,64) #valid servo number
+			and loc in range (1024) #valid destination
 			and spd in range (1024) #valid speed
 		) : 
-			loc = self.step(id, loc) #check if valid location
-			spd = self.time(loc) #make valid time
 			ax.moveSpeedRW(id, loc, spd) 
 			if debug : print "Servo: " + str(id) + " to loc: " + str(loc) + " at spd: " + str(spd)
 		else :
@@ -112,15 +113,27 @@ class Foot :
 		
 	def rest(self) :
 		loc = [0,0,0]
-		if self.legNr == 2 or self.legNr == 5 :
-			loc[0] = 0 
-			loc[1] = 90 #40.68
-			loc[2] = -45 #150 #120
-		elif self.legNr == 1 or self.legNr == 4 :
+		if self.legNr == 1 :
 			loc[0] = 45 
 			loc[1] = 90 #40.68
 			loc[2] = -45 #150 #120
-		elif self.legNr == 3 or self.legNr == 6 :
+		elif self.legNr == 2 :
+			loc[0] = 0 
+			loc[1] = 90 #40.68
+			loc[2] = -45 #150 #120
+		elif self.legNr == 3 : 
+			loc[0] = -45 
+			loc[1] = 90 #40.68
+			loc[2] = -45 #150 #120
+		elif self.legNr == 4 :
+			loc[0] = 45 
+			loc[1] = 90 #40.68
+			loc[2] = -45 #150 #120
+		elif self.legNr == 5 :
+			loc[0] = 0 
+			loc[1] = 90 #40.68
+			loc[2] = -45 #150 #120
+		elif self.legNr == 6 :
 			loc[0] = -45 
 			loc[1] = 90 #40.68
 			loc[2] = -45 #150 #120
@@ -135,9 +148,9 @@ class Foot :
 		self.moveSpeedRW(10*self.legNr+3, loc[2])
 		ax.action() # TODO do this here?
 		
+	#part 1b: van achter naar midden, door de lucht.
 	def stepOne(self, size):
-		loc = []
-		#part 1b: van achter naar midden, door de lucht.
+		loc = [0,0]
 		if self.legNr == 1 : 
 			loc[0] = 45 / size
 		elif self.legNr == 3 :
@@ -156,16 +169,15 @@ class Foot :
 		self.moveSpeedRW(10*self.legNr+1, loc[0])
 		self.moveSpeedRW(10*self.legNr+2, loc[1]) 
 
+	#part 1b: van midden naar voor, door de lucht.
 	def stepTwo(self, size) :
-		loc = []
-		#part 1b: van midden naar voor, door de lucht.
+		loc = [0,0]
 		if self.legNr == 1 : 
 			loc[0] = 9.73 / size
 		elif self.legNr == 3 :
 			loc[0] = 45 / size
 		elif self.legNr == 5 :
 			loc[0] = -30 / size
-			
 		elif self.legNr == 2 :
 			loc[0] = -9.73 / size
 		elif self.legNr == 4 :
@@ -177,9 +189,9 @@ class Foot :
 		self.moveSpeedRW(10*self.legNr+1, loc[0])
 		self.moveSpeedRW(10*self.legNr+2, loc[1]) 
 
+	#part 2a: van voor naar het midden, over de grond
 	def stepThree(self) :	
-		loc = []
-		#part 2a: van voor naar het midden, over de grond
+		loc = [0,0,0]
 		if self.legNr == 1 : 		
 			loc[0] = -9.73
 			loc[1] = 42.37
@@ -212,36 +224,36 @@ class Foot :
 	
 	#part 2b: van het midden naar achteren, over de grond.
 	def stepFour(self) :
-		loc = []
-		if legNr == 1 : 		
+		loc = [0,0,0]
+		if self.legNr == 1 : 		
 			loc[0] = -45
 			loc[1] = -34.86
 			loc[2] = -103.62
-		elif legNr == 3 :
+		elif self.legNr == 3 :
 			loc[0] = -9.73
 			loc[1] = -34.86
 			loc[2] = -103.62
-		elif legNr == 5 :
+		elif self.legNr == 5 :
 			loc[0] = 30
 			loc[1] = 40.68 + 1.81
 			loc[2] = 120 - 8.49	
-		elif legNr == 2 :
+		elif self.legNr == 2 :
 			loc[0] = -30
 			loc[1] = 40.68 + 1.81
 			loc[2] = 120 - 8.49
-		elif legNr == 4 :
+		elif self.legNr == 4 :
 			loc[0] = 9.73
 			loc[1] = -34.86
 			loc[2] = -103.62 
-		elif legNr == 6 :
+		elif self.legNr == 6 :
 			loc[0] = 45
 			loc[1] = -34.86
 			loc[2] = -103.62
-	moveSpeedRW(10*legNr+1, loc[0])
-	moveSpeedRW(10*legNr+2, loc[1]) 
-	moveSpeedRW(10*legNr+3, loc[2]) 
+		self.moveSpeedRW(10*self.legNr+1, loc[0])
+		self.moveSpeedRW(10*self.legNr+2, loc[1]) 
+		self.moveSpeedRW(10*self.legNr+3, loc[2]) 
 	
-def walking() :
+def walking(steps) :
 #method for all six legs, to walk.
 	if len(feet) < 6 :
 		print "ERROR! Not enough legs connected to walk!"
@@ -253,32 +265,32 @@ def walking() :
 #		for leg in feet: leg.set()
 #		ax.action()
 #		sleep(0.1)
-		for leg in feet: leg.go()
-		ax.action()
-		sleep(1.0)
+#		for leg in feet: leg.go()
+#		ax.action()
+#		sleep(1.0)
 		
 		while steps :
 			for leg in feet:
 				if(leg.legNr%2 == 0) : leg.stepFour()
-				else : leg.stepTwo()
+				else : leg.stepTwo(2)
 			ax.action()
 			sleep(0.1)
 			
 			for leg in feet:
-				if(leg.legNr%2 == 0) : leg.stepOne()
+				if(leg.legNr%2 == 0) : leg.stepOne(2)
 				else : leg.stepThree()
 			ax.action()
 			sleep(0.5)
 			
 			for leg in feet:
-				if(leg.legNr%2 == 0) : leg.stepTwo()
+				if(leg.legNr%2 == 0) : leg.stepTwo(2)
 				else : leg.stepFour()
 			ax.action()
 			sleep(0.1)
 			
 			for leg in feet:
 				if(leg.legNr%2 == 0) : leg.stepThree()
-				else : leg.stepOne()
+				else : leg.stepOne(2)
 			ax.action()
 			sleep(0.5)
 			
@@ -299,13 +311,19 @@ try :
 			only = "Only "
 	print only + str(len(feet)) + " legs initialized. Now to conquer the world."
 	
-	for leg in feet: leg.go()
-	ax.action()
-	sleep(1.0)
+#	for leg in feet: 
+#		leg.go()
+#		leg.rest()
+#	ax.action()
+
+	steps = 10
+#	steps = input("How many steps?")
 	
-	steps = input("How many steps?")
+	sleep(0.4)
 	walking(steps)
-	rest()
+	for leg in feet:
+		leg.rest()
+	ax.action()
 	
 except KeyboardInterrupt :
 	for leg in feet :
