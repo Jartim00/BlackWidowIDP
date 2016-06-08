d9aa7f6af44c3b61dc7415f558d47d298190fc4a
Jurjen <haze1401@student.nhl.nl>
2016-06-08 09:39:57 +0200

    performance fixes

--- Inverse_Kinematics/servospeedcalculator.py
+++ Inverse_Kinematics/servospeedcalculator.py
@@ -12,10 +12,8 @@ def speedPerSec(time,total_angle):
 def calculateSpeed(total_angle,time):
 	return (float(total_angle) / (float(time) / (1.614 / 1024.0))) * 512.0
 
-'''
-   Moves the servo to the endPos in the specified time.
-'''
-def moveInTime(id,endPos,time):
+def getSpeed(id,endPos,time):
+	speed = 512
 	if endPos > 1024 or endPos < 0:
 		raise IndexError("End position out of bounds")
 	curPos = ax.readPosition(id)
@@ -23,21 +21,29 @@ def moveInTime(id,endPos,time):
 	speed = calculateSpeed(totalAngle, time)
 	if speed > 512:
 		raise Exception("Time is to low")
-	ax.moveSpeed(id,endPos,int(speed))
+	return int(speed)
+'''
+   Moves the servo to the endPos in the specified time.
+'''
+def moveInTime(id,endPos,time):
+	speed = 512
+	try:
+		speed = getSpeed(id,endPos,time)
+	except:
+		raise
+	ax.moveSpeed(id,endPos,speed)
 
 '''
    Sets servo action for moving to the endPos given the time.
    Execute ax.action() for actually moving the servo.
 '''
 def moveInTimeRW(id,endPos,time):
-	if endPos > 1024 or endPos < 0:
-		raise IndexError("End position out of bounds")
-	curPos = ax.readPosition(id)
-	totalAngle = math.fabs(curPos - endPos)
-	speed = calculateSpeed(totalAngle, time)
-	if speed > 512:
-		raise Exception("Time is to low")
-	ax.moveSpeedRW(id,endPos,int(speed))
+	speed = 512
+	try:
+		speed = getSpeed(id,endPos,time)
+	except:
+		raise
+	ax.moveSpeedRW(id,endPos,speed)
 
 if __name__ == "__main__":
 	moveInTimeRW(13,500,1)
