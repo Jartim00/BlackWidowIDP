3dc75079ed51f2023556b369bd6b8da820eda45e
Jartim00 <jarontimmerman@live.nl>
2016-06-16 12:35:13 +0200

    modified:   src/movement/speedcalc.py

--- src/movement/speedcalc.py
+++ src/movement/speedcalc.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python
 import math
-##calculates speed based on gyro sensor value between -60 and 60
-#@param z Float representing x-value of the gyro-sensor
-def omrek(z):			#input: x-waarde
-	a = 400.0/60.0
-	if z >= 60:
-		z=60
-	elif z <= -60:
-		z=-60
-	elif z >= -10 and z <= 10:
-		z = 0
-	c = a*z
-	return math.fabs(c)
-	
 
-##calculates angle based on y-value of the gyro sensor
-#@param b Float representing y-value of the gyro-sensor
-def angleCalc(b):		#input: y-waarde
-	startPos = 511.0	
-	c=300/1024.0
-	angle = startPos + (b*0.75)/c
-	print angle
-	return angle
+class Speedcalculation:
+	##calculates speed based on gyro sensor's x-and-y-value between -60 and 60
+	#@param x Float representing x-value of the gyro-sensor
+	#@param y Float representing y-value of the gyro-sensor		
+	maxSpeed = 511
+	unit = maxSpeed/60.0
+	def speed(self,x,y):			#input: x-waarde
+		r = math.sqrt(x ** 2 + y ** 2)*self.unit
+		if r <= self.maxSpeed and r >= 120:
+			print r
+		elif r >= 120:
+			print self.maxSpeed
+		else:
+			print 0
 
-
-##connects the 2 previous methods so they can be used in the main class
-#@param a Float representing x-value of the gyro-sensor
-#@param b Float representing y-value of the gyro-sensor
-def beweging(a,b):
-	angleCalc(a)
-	omrek(b)
-
-omrek(-60)
-	
-	
-	
+	##calculates angle based on y-value of the gyro sensor
+	#@param b Float representing y-value of the gyro-sensor
+	def angleCalc(self,b):		#input: y-waarde
+		startPos = 511.0	
+		c=300/1024.0
+		angle = startPos + (b*0.75)/c
+		print angle
+		return angle
 
 
 
