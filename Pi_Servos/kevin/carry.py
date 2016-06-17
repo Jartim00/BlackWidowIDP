e82e144fe3adb1f00423e65ec96086b2293d2296
Diamundo <nautakevin@hotmail.com>
2016-06-17 14:47:10 +0200

    Nogmaals gedocumenteerd
    Vorige documentatie was niet doorgekomen. dan zo maar.

--- Pi_Servos/kevin/carry.py
+++ Pi_Servos/kevin/carry.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 from ax import Ax12
-from time import sleep
-import os
-
 ax = Ax12()
-list = ax.learnServos(10,64)
-_FACTOR = 0.29296875 #300/1024 -> omrekenfactor van graden naar stappen
 
-#ready positions
-	
-for legNr in range (1,7) :	
-	ax.moveSpeed(10*legNr+1, 512, 512)
-	ax.moveSpeed(10*legNr+2, int(512 + (90/_FACTOR)), 512)
-	ax.moveSpeed(10*legNr+3, int(512 - (45/_FACTOR)), 512)
+## Draagmodus van de spin, waarmee de spin vervoerd en gedragen kan worden.                                                          
+def draagmodus():
+	_FACTOR = 0.29296875 #300/1024 -> omrekenfactor van graden naar stappen	
+	for legNr in range (1,7) :	
+		ax.moveSpeed(10*legNr+1, 512, 512)
+		ax.moveSpeed(10*legNr+2, int(512 + (90/_FACTOR)), 512)
+		ax.moveSpeed(10*legNr+3, int(512 - (45/_FACTOR)), 512)
 	ax.action()
