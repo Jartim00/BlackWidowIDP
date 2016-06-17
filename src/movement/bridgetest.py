5ec6817ac6d8c3a1e43faee24a2e7b6944fe6358
Sylvius <sylviusroeles@hotmail.com>
2016-06-17 15:35:18 +0200

    Bridgetest modified

--- src/movement/bridgetest.py
+++ src/movement/bridgetest.py
@@ -13,60 +13,61 @@ class Bridge:
     def readyForBridge(self):
         isMoving = True
         loc = [0,0]
-            for x in range(1,7):
-                loc = bx.calc_angles(0, 84.24478972, 0)
-                ax.moveSpeedRW(x*10 + 2, loc[0], 450)
-                ax.moveSpeedRW(x*10 + 3, loc[1], 450)
-                if leg == 2 or leg == 5:
-                    ax.moveSpeedRW(x*10 + 1, 512, 450)
-                elif leg ==  1 or leg == 4:
-                    ax.moveSpeedRW(x*10 + 1, bx.AngleToServo(45), 450)
-                else:
-                    ax.moveSpeedRW(x*10 + 1, bx.AngleToServo(-45), 450)
+        for leg in range(1,7):
+            loc = bx.calc_angles(0, 84.24478972, 0)
+            ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
+            ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
+            if leg == 2 or leg == 5:
+                ax.moveSpeedRW(leg*10 + 1, 512, 450)
+            elif leg ==  1 or leg == 4:
+                ax.moveSpeedRW(leg*10 + 1, bx.AngleToServo(60), 450)
+            elif leg == 1 or leg == 6:
+                loc = bx.calc_angles(0, 120, 0)
+                ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
+                ax.moveSpeedRW(leg*10 + 3, loc[1], 450)                
+            else:
+                ax.moveSpeedRW(leg*10 + 1, bx.AngleToServo(-60), 450)
         isMoving = False
         ax.action()
-	
-    ##Push the rear legs to slide across the bridge
-    def pushBackLegs(self):
-	isMoving = True
-	loc = bx.calc_angles(0, 119.24478972, 5)
-	#push legs
-	for leg in range(3,4):
+
+    def rups(self):
+        #raise from ground
+        for x in range(1,7,5):
+            #front legs 1 and 6
+            loc = bx.calc_angles(0, 120, 30)
+            ax.moveSpeedRW(x*10 + 2, loc[0], 450)
+            ax.moveSpeedRW(x*10 + 3, loc[1], 450)
+        for x in range(3,4):
+            #rear legs 3 and 4
+            loc = bx.calc_angles(0, 84.24478972, 30)
             ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
-	    ax.moveSpeedRW(leg*10 + 3, loc[1], 450)	
-	sleep(0.1)
-	#raise legs
-	loc = bx.calc_angles(0, 84.24478972, 0)
-	for leg in range(3,4):
-	    ax.moveSpeedRW(leg*10 + 2, loc[0] + 50, 450)
-	    ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
-	sleep(0.1)
-	#lower legs
-        for leg in range(3,4):
-	    ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
-	    ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
-	sleep(0.1)
-	isMoving = False
-	
-	##Pulls the front legs to slide across the bridge
-    def pushFrontLegs(self):
-	isMoving = True
-	#raise legs
-	loc = bx.calc_angles(0, 119.24478972, 0)
-	for leg in range(1,7,5):
-	    ax.moveSpeedRW(leg*10 + 2, loc[0] + 50, 450)
-	    ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
-	sleep(0.1)
-	#lower legs
-        for leg in range(1,7,5):
-	    ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
-	    ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
-	sleep(0.1)	
-	#pull legs
-	loc = bx.calc_angles(0, 84.24478972, 5)
-	for leg in range(1,7,5):
-	    ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
-	    ax.moveSpeedRW(leg*10 + 3, loc[1], 450)	
-	sleep(0.1)	
-        isMoving = False
+            ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
+        ax.action()
+        sleep(0.2)
+        #push spider forward
+        for x in range(1,7,5):
+            #front legs 1 and 6
+            loc = bx.calc_angles(0, 84.24478972, 30)
+            ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
+            ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
+        for x in range(3,4):
+            #rear legs 3 and 4
+            loc = bx.calc_angles(0, 120, 30)
+            ax.moveSpeedRW(x*10 + 2, loc[0], 450)
+            ax.moveSpeedRW(x*10 + 3, loc[1], 450)
+        ax.action()
+        sleep(0.5)
+        for x in range(1,7,5):
+            #front legs 1 and 6
+            loc = bx.calc_angles(0, 120, 0)
+            ax.moveSpeedRW(x*10 + 2, loc[0], 450)
+            ax.moveSpeedRW(x*10 + 3, loc[1], 450)
+        for x in range(3,4):
+            #rear legs 3 and 4
+            loc = bx.calc_angles(0, 84.24478972, 0)
+            ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
+            ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
+        ax.action()
+        sleep(0.2)
+        
 		
