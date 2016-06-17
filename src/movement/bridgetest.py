89b6c1a5b7ec90b9a31fb58042b87e17b950f4b9
Sylvius <sylviusroeles@hotmail.com>
2016-06-17 12:07:31 +0200

    Bridge Test bug fixed

--- src/movement/bridgetest.py
+++ src/movement/bridgetest.py
@@ -54,18 +54,18 @@ class Bridge:
 		isMoving = True
 		#raise legs
 		loc = bx.calc_angles(0, 119.24478972, 0)
-		for x in range(3,4):
+		for x in range(1,7,5):
 			ax.moveSpeedRW(leg*10 + 2, loc[0] + 50, 450)
 			ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
 		sleep(0.1)
 		#lower legs
-        for x in range(3,4):
+        for x in range(1,7,5):
 			ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
 			ax.moveSpeedRW(leg*10 + 3, loc[1], 450)
 		sleep(0.1)	
 		#pull legs
 		loc = bx.calc_angles(0, 84.24478972, 5)
-		for x in range(3,4):
+		for x in range(1,7,5):
 			ax.moveSpeedRW(leg*10 + 2, loc[0], 450)
 			ax.moveSpeedRW(leg*10 + 3, loc[1], 450)	
 		sleep(0.1)	
