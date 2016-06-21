58cbbf492303de7dfc3002293b049eae73f0978a
Jurjen <haze1401@student.nhl.nl>
2016-06-21 11:24:04 +0200

    camera flip uit gezet

--- src/camera_pi.py
+++ src/camera_pi.py
@@ -1,4 +1,4 @@
-import time
+ import time
 import io
 import threading
 import picamera
@@ -36,8 +36,8 @@ class Camera(object):
         with picamera.PiCamera() as camera:
             # camera setup
             camera.resolution = (cls.width, cls.height)
-            camera.hflip = True
-            camera.vflip = True
+            #camera.hflip = True
+            #camera.vflip = True
 
             # let camera warm up
             camera.start_preview()
